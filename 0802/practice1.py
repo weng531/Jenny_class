81f5681030663ea240eb8c1814058c6b54228f34
Jenny <weng131063@gmail.com>
2026-08-09 11:28:20 +0800

    第一次提交

---
+++ 0802/practice1.py
@@ -0,0 +1,8 @@
+print("Hello, Python!")          # 英文
+print("你好，Python！")            # 中文
+print("こんにちは、Python！")      # 日文
+print("안녕하세요, Python!")       # 韓文
+print("Bonjour, Python!")         # 法文
+print("Hola, Python!")            # 西班牙文
+print("Ciao, Python!")            # 義大利文
+print("Hallo, Python!")           # 德文
